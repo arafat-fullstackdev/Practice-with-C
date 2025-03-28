418e102b6ddb1d5712423ab5b2a5a9a0ae0d0d9c
Arafat Patwary <farabipsk@gmail.com>
2025-03-28 19:56:22 +0600

    Add C input

---
+++ Intermediate/input.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+   int value;
+   //Take input from user
+   scanf("%d",&value);
+   printf("Please give a number\n");
+   //printf("%d",value);
+
+   float marks;
+   printf("Given marks\n");
+   scanf("%f",&marks);
+   //printf("Please give a marks\n%f",marks);
+    
+   char ch;
+   printf("Please select Character\n");
+   scanf("%f",&ch);
+
+   printf("You have char%c",ch);
+     
+    return 0;
+}
