702cbc1a36f55a35739f4301f29b22cbcaddef69
Arafat Patwary <farabipsk@gmail.com>
2025-03-28 15:14:34 +0600

    Define variable

---
+++ Intermediate/new.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int age;
+    int name;
+    age = 24;
+    int cgpa = 3.5;
+    //name = "HongKong"
+    printf("My roll is %d\n",age);
+    printf("My GPA is%d\n",cgpa);
+
+    // Define Variable
+    int price_of_chocolate = 30;
+    int price_of_cheaps = 12;
+    int priceOfVegetable = 60;
+
+    printf("Chocolate price: %d\n",price_of_chocolate);
+    printf("Cheaps price: %d\n",price_of_cheaps);
+    printf("Vegetable price: %d\n",priceOfVegetable);
+
+    return 0;
+}
