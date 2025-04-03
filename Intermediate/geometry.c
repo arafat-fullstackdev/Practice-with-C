df8c12c3c49b77e7d20c9e42d55f214cab80db2e
Arafat Patwary <farabipsk@gmail.com>
2025-04-03 10:14:23 +0600

    Second Problem solve

---
+++ Intermediate/geometry.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float pi = 3.14;
+    int radius;
+    float area;
+   printf("Enter Radius: ");
+    scanf("%d",&area);
+
+    area = pi * radius *radius;
+
+    printf("Area of Place:%2.f",area);
+
+    return 0;
+}
