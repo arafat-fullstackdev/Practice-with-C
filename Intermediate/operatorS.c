7883c647574446c2943c9c6aa8cccdd90593d771
Arafat Patwary <farabipsk@gmail.com>
2025-04-05 15:27:32 +0600

    New OPerator Statement

---
+++ Intermediate/operatorS.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+   int a, b;
+   a=10;
+   b=12;
+   printf("%d\n", a==b);   
+   printf("%d\n", a>b);   
+   printf("%d\n", a<b);   
+   printf("%d\n", a<=b);   
+   printf("%d\n", a>=b);   
+   printf("%d\n", a!=b);   
+    return 0;
+}
