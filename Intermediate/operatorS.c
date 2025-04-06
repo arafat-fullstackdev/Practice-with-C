476c21d2d53d9f7445899a9ac7af3ac68b387670
Arafat Patwary <farabipsk@gmail.com>
2025-04-06 09:34:09 +0600

    Done simple markes sheet

--- Intermediate/operatorS.c
+++ Intermediate/operatorS.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
 
 int main() {
-   int a, b;
-   a=10;
-   b=12;
-   printf("%d\n", a==b);   
-   printf("%d\n", a>b);   
-   printf("%d\n", a<b);   
-   printf("%d\n", a<=b);   
-   printf("%d\n", a>=b);   
-   printf("%d\n", a!=b);   
+//    int a, b;
+//    a=10;
+//    b=12;
+//    printf("%d\n", a==b);   
+//    printf("%d\n", a>b);   
+//    printf("%d\n", a<b);   
+//    printf("%d\n", a<=b);   
+//    printf("%d\n", a>=b);   
+//    printf("%d\n", a!=b);    
+
+int marks;
+scanf("%d", &marks);
+
+ if(marks>85){
+    printf("Congratulations!");
+}else if(marks> 75){
+    printf("Good Job");
+}else if(marks>65){
+    printf("You can do better");
+}
+else{
+    printf("Try again");
+}
+
     return 0;
 }
