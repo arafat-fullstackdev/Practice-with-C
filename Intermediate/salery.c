0a3e7059d9f15086ec266573d8f61d5c86166c1c
Arafat Patwary <farabipsk@gmail.com>
2025-04-17 09:21:28 +0600

    Add array size and size

---
+++ Intermediate/salery.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+  int salary[5];
+  int bonus;
+  int i;
+
+ // printf("Give me Bonus amount:");
+  //scanf("%d", &bonus);
+
+  for(i=0; i<5; i++){
+    //scanf("%d", &salary[i]);
+  }
+
+  for(
+    i=0;i<5; i++){
+  salary[i] =salary[i] +bonus;
+
+  //printf("Total salary: %d\n", salary[i]);
+  }
+
+
+  //Aray size
+  //char name=["LalMia"];
+  char nameS;
+//   nameS= "LalMIa";
+  //printf("%d", sizeof(name));
+  printf("The size of value: %c", sizeof(nameS));
+
+  float marks;
+  printf("Float : %d", sizeof(marks));
+     
+    return 0;
+}
