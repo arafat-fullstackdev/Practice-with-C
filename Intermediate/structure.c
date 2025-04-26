116af02b454be4f9eb458ff873baf38b769d8286
Arafat Patwary <farabipsk@gmail.com>
2025-04-26 10:53:18 +0600

    Create Employe sheet

--- Intermediate/structure.c
+++ Intermediate/structure.c
@@ -9,17 +9,39 @@
     int grade;
    };
 
+   struct Employee{
+    char name[100];
+    char address[100];
+    int id;
+    int grade;
+   };
+
  int main()
   {
-    int a;
+    // int a;
 
-    struct student std;
-    std.id = 04;
-    std.grade = 3;
+    // struct student std;
+    // std.id = 04;
+    // std.grade = 3;
 
-    strcpy( std.name, "Kamal");
-    strcpy( std.address, "Chittagong");
-    printf("%s %s %d %d\n", std.name,std.address, std.grade,std.id);
+    // strcpy( std.name, "Kamal");
+    // strcpy( std.address, "Chittagong");
+    // printf("%s %s %d %d\n", std.name,std.address, std.grade,std.id);
+
+    struct Employee emp;
+
+    printf("Enter Employee name:");
+    scanf("%s", &emp.name);
+    //printf("%s\n", emp.name);
+ printf("Enter Employee address:");
+    scanf("%s\n", &emp.address);
+    strcpy(emp.address, "Irkutusk");
+    printf("Enter Employee grade:");
+    scanf("%d\n", &emp.grade);
+
+    printf("Employee name : %s\n",emp.name);
+    printf("Employee Address : %s\n",emp.address);
+    printf("Employee grade : %d\n",emp.grade);
     
     return 0;
 }
