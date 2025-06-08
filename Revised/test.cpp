f243062dcdcf448a2a40bbb2d49f95f5a91d91cc
Arafat Patwary <farabipsk@gmail.com>
2025-06-08 16:39:08 +0600

    Code revised

---
+++ Revised/test.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+ public:
+    // constructor
+    Solution(){
+        cout<<"Hello Constructor!"<<endl;
+    }
+};
+int main(){
+    // Creating object
+    Solution obj;
+    return 0;
+
+}
